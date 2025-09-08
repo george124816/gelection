73bfaafda3f96932d7b5678eccb90011e2dec96b
George Rodrigues <georgerodrigues@live.com.au>
2025-09-08 20:06:32 -0300

    feat: connect to kafka (#6)

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v5"
+	"log"
 	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	kafka "github.com/segmentio/kafka-go"
 )
 
 func main() {
+	// postgres example
+	helloWorldDatabase()
+
+	// kafka example
+	publishMessage()
+	consumeMessage()
+}
+
+func helloWorldDatabase() {
 	conn, err := pgx.Connect(context.Background(), "postgres://postgres:postgres@localhost:5555/postgres")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database %v\n", err)
@@ -23,4 +36,67 @@ func main() {
 	}
 
 	fmt.Println(greeting)
+}
+
+func publishMessage() {
+	topic := "vote"
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader: ", err)
+
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("three!")},
+	)
+
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer: ", err)
+
+	}
+
+	fmt.Println("oi")
+}
+
+func consumeMessage() {
+	topic := "vote"
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+
+	if err != nil {
+		log.Fatal("failed to dial leader: ", err)
+
+	}
+
+	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	batch := conn.ReadBatch(10e3, 1e6)
+
+	b := make([]byte, 10e3)
+	for {
+		n, err := batch.Read(b)
+		if err != nil {
+			break
+		}
+
+		fmt.Println(string(b[:n]))
+	}
+
+	if err := batch.Close(); err != nil {
+		log.Fatal("failed to close batch:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close connection", err)
+	}
 }
