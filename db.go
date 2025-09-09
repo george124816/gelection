b515860be17a57e9cb6c8b62b257d9456330e47f
Joao Victor <joaoaraujo.367@gmail.com>
2025-09-09 19:49:45 -0300

    refactor: db connect using connection pool (#8)

---
+++ db.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+)
+
+var Db *pgxpool.Pool
+
+func Connect() {
+	poolConfig, err := pgxpool.ParseConfig("postgres://postgres:postgres@localhost:5555/postgres")
+	if err != nil {
+		log.Fatalln("Unable to parse DATABASE_URL:", err)
+	}
+
+	Db, err = pgxpool.NewWithConfig(context.Background(), poolConfig)
+	if err != nil {
+		log.Fatalln("Unable to create connection pool", err)
+	}
+}
