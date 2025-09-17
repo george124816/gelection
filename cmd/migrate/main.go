31cd08f4fceb3af9ad095652239cde5e44cc03d0
George Rodrigues <georgerodrigues@live.com.au>
2025-09-16 21:50:10 -0300

    feat: create migrate command (#12)

---
+++ cmd/migrate/main.go
@@ -0,0 +1,29 @@
+package migrate
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+func Migrate() {
+	db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5555/postgres?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://db/migrations",
+		"postgres", driver)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m.Up()
+}
