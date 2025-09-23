696229780a6da8d8d9513edf475457ff3022fc47
George Rodrigues <georgerodrigues@live.com.au>
2025-09-22 22:18:36 -0300

    chore: create config to store envs

--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -1,6 +1,11 @@
 package configs
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+)
 
 type HttpConfig struct {
 	Port uint16
@@ -9,4 +14,50 @@ type HttpConfig struct {
 func (h *HttpConfig) GetStringPort() string {
 	return fmt.Sprintf(":%d", h.Port)
 
+}
+
+type DatabaseConfig struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	Database string
+	SslMode  string
+}
+
+func (c DatabaseConfig) String() string {
+	pgUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", c.User, c.Password, c.Host, c.Port, c.Database, c.SslMode)
+	u, err := url.Parse(pgUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return u.String()
+}
+
+func GetPostgressConfig() DatabaseConfig {
+	user := GetEnvOrDefault("DATABASE_USER", "postgres")
+	password := url.QueryEscape(GetEnvOrDefault("DATABASE_PASSWORD", "postgres"))
+	host := GetEnvOrDefault("DATABASE_HOST", "localhost")
+	port := GetEnvOrDefault("DATABASE_PORT", "5555")
+	database := GetEnvOrDefault("DATABASE_NAME", "postgres")
+	sslMode := GetEnvOrDefault("DATABASE_SSL_MODE", "disable")
+
+	return DatabaseConfig{
+		User:     user,
+		Password: password,
+		Host:     host,
+		Port:     port,
+		Database: database,
+		SslMode:  sslMode,
+	}
+
+}
+
+func GetEnvOrDefault(envName string, defaultValue string) string {
+	resultValue := os.Getenv(envName)
+	if resultValue != "" {
+		return resultValue
+	} else {
+		return defaultValue
+	}
 }
