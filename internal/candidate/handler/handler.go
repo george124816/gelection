b31a04319819b94499510a9196350a1e19e3545c
George Rodrigues <georgerodrigues@live.com.au>
2025-09-23 19:54:58 -0300

    fix: return error when cant decode json (#29)
    * fix: return error when cant decode json
    
    * fix: add more returns
    
    * fix: adjust http status

--- internal/candidate/handler/handler.go
+++ internal/candidate/handler/handler.go
@@ -19,12 +19,14 @@ func CandidateRetrieveUpdateDestroyHandler(w http.ResponseWriter, r *http.Reques
 	case r.Method == "GET":
 		inputId, err := strconv.Atoi(r.PathValue("id"))
 		if err != nil {
+			log.Println(err)
 			fmt.Fprintln(w, err)
 		}
 
 		candidate, err := repository.GetCandidate(context.Background(), engine.Engine, uint64(inputId))
 		result, err := json.Marshal(candidate)
 		if err != nil {
+			log.Println(err)
 			fmt.Fprintln(w, err)
 		}
 		fmt.Fprintln(w, string(result))
@@ -32,6 +34,7 @@ func CandidateRetrieveUpdateDestroyHandler(w http.ResponseWriter, r *http.Reques
 		var requestCandidate model.Candidate
 		inputId, err := strconv.Atoi(r.PathValue("id"))
 		if err != nil {
+			log.Println(err)
 			fmt.Fprintln(w, err)
 			return
 		}
@@ -76,17 +79,24 @@ func CandidateListCreateHandler(w http.ResponseWriter, r *http.Request) {
 
 		if err != nil {
 			log.Println("body request failed")
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, err)
+			return
 		}
 
 		err = json.Unmarshal(bodyRequest, &requestCandidate)
 
 		if err != nil {
 			log.Println("failed to decode json")
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, err)
+			return
 		}
 
 		err = repository.Create(requestCandidate)
 
 		if err != nil {
+			log.Println("failed to create candidate")
 			w.WriteHeader(http.StatusConflict)
 			fmt.Fprintln(w, err)
 			return
