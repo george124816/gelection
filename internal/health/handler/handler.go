9b68d5db1027f2192587249d238e4aaa4133f6b6
Joao Victor <joaoaraujo.367@gmail.com>
2025-10-02 19:10:49 -0300

    feat: health check handler (#48)
    * feat: add health check
    
    * feat: fix imports

---
+++ internal/health/handler/handler.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == "GET":
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "OK")
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method Not Allowed")
+	}
+}
